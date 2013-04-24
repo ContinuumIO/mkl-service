6549223c4eced5e03405715d8b2975f6359a9d96
Ilan Schnell <ilanschnell@gmail.com>
2013-04-23 20:31:57 -0500

    add changelog file, add version

--- mkl/__init__.py
+++ mkl/__init__.py
@@ -7,7 +7,7 @@ from .service import (get_version_string, mem_stat, get_cpu_clocks,
                       get_cpu_frequency, set_num_threads, get_max_threads)
 
 
-__version__ = '__VERSION__'
+__version__ = '1.0'
 
 
 def test(verbosity=1):
