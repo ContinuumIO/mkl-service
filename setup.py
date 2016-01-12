a998443658b93955c4c416201ed0dc987fcd78ca
Ilan Schnell <ilanschnell@gmail.com>
2016-01-11 18:06:09 -0600

    update MKL location on Windows

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 
+if sys.platform == 'win32':
+    mkl_prefix = join(sys.prefix, 'Library')
+else:
+    mkl_prefix = sys.prefix
+
 ext_kwds = dict(
     name = "mkl.service",
     sources = ["mkl/service.c"],
     libraries = ["mkl_rt"],
     define_macros = [],
-    include_dirs = [join(sys.prefix, 'include')],
-    library_dirs = [join(sys.prefix,
-                         'libs' if sys.platform == 'win32' else 'lib')]
+    include_dirs = [join(mkl_prefix, 'include')],
+    library_dirs = [join(mkl_prefix, 'lib')],
 )
 
 setup(
